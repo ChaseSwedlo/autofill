3cb539922b0b3eb1b1ddbae63c722f2e100c2f43
Chase Swedlo <chasechaseswedlo@outlook.com>
2024-03-25 13:55:31 -0500

    Add buildMovieInfo

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,6 +3,7 @@ import movies from '../data/movies.js';
 
 const userInput = document.querySelector('.user-input');
 const movieList = document.querySelector('.movie-lis');
+const lis = document.querySelectorAll('li');
 
 function searchMovies(val) {
     let inputVal = val;
@@ -44,3 +45,40 @@ userInput.addEventListener('input', () => {
     const arr = searchMovies(userIn);
     list(userIn);
 });
+
+function findIndex(title) {
+    for(let i = 0; i < movies.length; i++) {
+        if(title === movies[i].title)
+            return i;
+    }
+}
+
+const movPoster = document.querySelector('.poster');
+const movTitle = document.querySelector('.title');
+const movDescription = document.querySelector('.description');
+const movTime = document.querySelector('.time');
+function buildMovieInfo(title) {
+    let index = findIndex(title);
+    console.log(index);
+    let imgURL = movies[index].poster;
+    console.log(imgURL);
+    movPoster.style.backgroundImage = `url(${imgURL})`;
+    movTitle.innerText = movies[index].title;
+    movDescription.innerText = movies[index].description;
+    movTime.innerText = movies[index].runningTime;
+}
+
+function clearInput() {
+    userInput.value = '';
+    movieList.innerHTML = '';
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    movieList.addEventListener("click", (event) => {
+      if (event.target.tagName === "LI") {
+        let selectedTitle = event.target.innerText;
+        buildMovieInfo(selectedTitle);
+        clearInput();
+      }
+    });
+  });
